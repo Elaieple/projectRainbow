9451d126409e0b29a6300af6ed248ac2ff3c8ac9
Shakhjah <shakhprodukti@mail.ru>
2022-12-12 09:27:10 +0300

    Release 3

--- client/src/components/KakPomoch/KakPomoch.js
+++ client/src/components/KakPomoch/KakPomoch.js
@@ -15,6 +15,76 @@ export default function KakPomoch() {
           <img src="kartinka2.png" alt="photo1" />
         </div>
       </div>
+      <div className='TextPlusBlok' >
+        <div className='TextplusStrelki'>
+        <div className='Blagodarya'>БЛАГОДАРЯ ВАШЕЙ ПОМОЩИ МЫ СМОЖЕМ</div>
+        <div className='Strelki' >
+        <div className='StrelkaLeft'>←</div>
+        <div className='StrelkaRight'>→</div>
+        </div>
+        </div> 
+        <div className='blokks' >
+            <div className='blokk1' >
+              <div className='blokk1txt'>
+              <div className='bloktxt1' >ВЫЯВИТЬ</div>
+              <div className='bloktxt12' >способности и склонности детей. В школе большое внимание будет уделяться различным видам деятельности, благодаря которым у детей появится возможность попробовать себя в новых ролях и найти интересные для себя занятия.</div>
+              </div>
+            </div>
+            <div className='blokk2' >
+              <div className='blokk2txt'>
+                <div className='bloktxt2'>СОЗДАТЬ</div>
+                <div className='bloktxt22'>для каждого ребенка свой образовательный маршрут, чтобы раскрыть его способности и дать те навыки и умения, которые помогут ему во взрослой жизни стать самостоятельной личностью.</div>
+              </div>
+            </div>
+            <div className='blokk3' >
+              <div className='blokk3txt'>
+                <div className='bloktxt3'>ПОМОЧЬ</div>
+                <div className='bloktxt33'>детям примерить на себя совершенно незнакомую пока роль. Из маленького опекаемого существа они сами превратятся в помощников, их внимание переключится на потребности и нужды других детей, они сами смогут стать нужными и полезными кому-то.</div>
+                </div>
+            </div>
+            <div className='blokk4'>
+            <div className='blokk4txt'>
+                <div className='bloktxt4'>ПОЗНАКОМИТЬ</div>
+                <div className='bloktxt44'>детей друг с другом и научить их вести себя в коллективе: общаться, договариваться, находить точки соприкосновения, учитывать интересы другого, справляться с конфликтными ситуациями, узнавать о внутреннем мире и интересах других детей. Научиться дружить и сохранять эти отношения надолго.</div>
+              </div>
+            </div>
+        </div>
+        <div className='SecondBlok'>
+          <div className='SecondBlokTexts' >
+          <div className='SecondBloktxt1'>НАМ НУЖНА ВАША <br/> ПОДДЕРЖКА</div>
+    
+          <div className='SecondBloktxt2'>
+            <p className='texxxt' >Чтобы совершить пожертвование, воспользуйтесь любым удобным для вас способом оплаты: перевод с банковской карты или электронные деньги через Яндекс.Деньги.
+Все платежи проходят через защищённую систему (подробнее).
+Правила возврата платежей можно прочитать здесь.</p>
+          <button className='SecondButton'>ПОМОЧЬ СЕЙЧАС</button>
+</div>
+          </div>
+          </div>
+          <div className='ThirdBlok'>
+            <div className='ThirdBlokTexts'>
+            <div className='ThirdBloktxt1'>ЧЕМ ПОМОЧЬ КРОМЕ ДЕНЕГ?</div>
+            <div className='ThirdBloktxt2'>
+              <div className='rowPlusplus1' >
+              <div className='row1 txt' >МАТЕРИАЛЬНАЯ ПОМОЩЬ</div>
+              <div className='plus1 plus'>+</div>
+              </div>
+              <div className='rowPlusplus2' >
+              <div className='row2 txt' >ПОМОЩЬ СПЕЦИАЛИСТОВ</div>
+              <div className='plus2 plus'>+</div>
+              </div>
+              <div className='rowPlusplus3' >
+              <div className='row3 txt' >ВОЛОНТЕРСКАЯ ПОМОЩЬ</div>
+              <div className='plus3 plus'>+</div>
+              </div>
+              <div className='rowPlusplus3' >
+              <div className='row4 txt' >ИНФОРМАЦИОННАЯ ПОМОЩЬ</div>
+              <div className='plus4 plus'>+</div>
+              </div>
+            </div>
+            </div>
+          </div>
+       </div>
 
 
     </>
